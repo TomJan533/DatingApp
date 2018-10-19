8aa11d140c4c1c3642ddc774e75976c902be29b7
TomekJ <tomekjankiewicz@gmail.com>
2018-10-19 18:41:20 +0200

    service AuthRepository added to configuration

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -31,6 +31,7 @@ namespace DatingApp.API
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
             services.AddCors();
+            services.AddScoped<IAuthRepository, AuthRepository>(); //AddScoped - initialized once per HTTP request
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
