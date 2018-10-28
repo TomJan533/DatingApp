d7d5070b9ad9d917361fa34d4d2df6627c049e6e
TomekJ <tomekjankiewicz@gmail.com>
2018-10-28 09:50:01 +0100

    logout dropdown with ngx-bootstrap, links visible after login

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { FormsModule} from '@angular/forms';
+import { BsDropdownModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { HttpClient } from 'selenium-webdriver/http';
@@ -11,6 +12,7 @@ import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { ErrorIntereptorProvider } from './_services/error.interceptor';
 import { AlertifyService } from './_services/alertify.service';
+
 
 @NgModule({
    declarations: [
@@ -22,7 +24,8 @@ import { AlertifyService } from './_services/alertify.service';
    imports: [
       BrowserModule,
       HttpClientModule,
-      FormsModule
+      FormsModule,
+      BsDropdownModule.forRoot()
    ],
    providers: [
       AuthService,
