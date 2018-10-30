76518fc6219c9a99fbfb931be1d61114160bd9fc
TomekJ <tomekjankiewicz@gmail.com>
2018-10-30 06:46:27 +0100

    UsersControler.cs with json error fixed

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -36,7 +36,11 @@ namespace DatingApp.API
         {
 
             services.AddDbContext<DataContext>(x => x.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1)
+                .AddJsonOptions(opt => {
+                    opt.SerializerSettings.ReferenceLoopHandling = 
+                        Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+                });
             services.AddCors();
             services.AddTransient<Seed>();
             services.AddScoped<IAuthRepository, AuthRepository>(); //AddScoped - initialized once per HTTP request
